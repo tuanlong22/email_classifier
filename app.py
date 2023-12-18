6262acc74fab1b7e7fb9924d587394dcb690e3a8
tuanlong22 <111887781+tuanlong22@users.noreply.github.com>
2023-12-18 22:42:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import pickle
 import streamlit as st
-
+from streamlit_option_menu import option_menu
+import json 
 
 model = pickle.load(open("spam.pkl","rb"))
 cv = pickle.load(open("vectorizer.pkl","rb"))
 
-
+st.set_page_config(
+    page_title="Spam Email Detector App",
+    page_icon="📫",
+)
 def main():
-    st.title("Email Spam Classification Apps")
-    st.subheader("build With Streamlit and Python")
+    st.title("Hệ thống nhận diện Spam Email :envelope_with_arrow:")
+    st.sidebar.success("Select a page above.")
+    st.subheader("Thực hiện bởi nhóm 6 :two_hearts:")
     msg = st.text_input("Enter a Text:")
-    if st.button("Predict"):
+    if st.button("Dự đoán"):
         data = [msg]
         vect = cv.transform(data).toarray()
         prediction = model.predict(vect)
         result = prediction[0]
         if result == 1:
-            st.error("This is a Spam Mail")
+            st.error("Đây là Spam Mail :outbox_tray:")
         else:
-            st.success("This is a Ham Mail")
+            st.success("Đây không phải là spam mail :incoming_envelope:")
+
 main()
